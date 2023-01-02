dfe2a24d5109c645753ea049c6dec8ee191fe352
Sara Raley <sara.m.raley@gmail.com>
2023-01-02 12:09:07 -0700

    adds users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+### CONDITIONS
+
+### FAST FACTS
+
+### HIKE SCHEDULES
+
+### TRAILS
+
+### USERS
+User.create!(name: "Packy the Pika", email: "p@example.com", password: "password", image_url: "https://files.slack.com/files-pri/T042JPJ3AAD-F04G1LB5ACS/image.png")
+
+User.create!(name: "Screech the Marmot", email: "m@example.com", password: "password", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhv4XlaHH3x-fekrC77ijAZA6WrwAnp0oGWA&usqp=CAU")
+
+User.create!(name: "Hops the Mountain Goat", email: "g@example.com", password: "password", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRA5P1wgcEmEFdSzhwTyqI9Ddb9FMeZTx7Ig&usqp=CAU")
