8698c0cd10085a37bde07b96a2b3b869049a9716
Sara Raley <sara.m.raley@gmail.com>
2023-01-02 16:45:52 -0700

    removes user update function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users.as_json
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render json: @user.as_json
+  end
+
   def create
     user = User.new(
       name: params[:name],
@@ -11,5 +21,11 @@ class UsersController < ApplicationController
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: { message: "User destroyed successfully" }
   end
 end
