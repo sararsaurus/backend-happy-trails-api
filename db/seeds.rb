05993f94030b593efafbc42f5b2349e698577aff
Sara Raley <sara.m.raley@gmail.com>
2023-01-02 12:25:41 -0700

    adds trails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,15 @@
 ### HIKE SCHEDULES
 
 ### TRAILS
+Trail.create!(name: "Arapahoe Pass and Lake Dorothy Trail", description: "Out and back", length: 7, difficulty: "Moderate", image_url: "https://files.slack.com/files-pri/T042JPJ3AAD-F04H75Z2C91/5635e15a-e79f-43fa-a0f8-de8ffe07f272_1_105_c.jpeg")
+
+Trail.create!(name: "South Arapaho Peak", description: "Out and back", length: 13, difficulty: "Hard", image_url: "https://files.slack.com/files-pri/T042JPJ3AAD-F04H4B5V6ER/720efad9-987d-4257-b412-fe7cdb139cf7_1_105_c.jpeg")
+
+Trail.create!(name: "Lone Eagle", description: "Out and back", length: 17, difficulty: "Moderate", image_url: "https://files.slack.com/files-pri/T042JPJ3AAD-F04H0MNDXJA/fd4d267a-8ce6-474f-90d2-b8c8936bbd58_1_105_c.jpeg")
 
 ### USERS
 User.create!(name: "Packy the Pika", email: "p@example.com", password: "password", image_url: "https://files.slack.com/files-pri/T042JPJ3AAD-F04G1LB5ACS/image.png")
 
 User.create!(name: "Screech the Marmot", email: "m@example.com", password: "password", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhv4XlaHH3x-fekrC77ijAZA6WrwAnp0oGWA&usqp=CAU")
 
-User.create!(name: "Hops the Mountain Goat", email: "g@example.com", password: "password", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRA5P1wgcEmEFdSzhwTyqI9Ddb9FMeZTx7Ig&usqp=CAU")
+User.create!(name: "Hops the Mountain Goat", email: "g@example.com", password: "password", image_url: "https://files.slack.com/files-pri/T042JPJ3AAD-F04HX0U5GCQ/dd68494e-b385-4cd9-bbbe-06f2cda2faee_1_105_c.jpeg")
