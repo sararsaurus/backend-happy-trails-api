58236337b018336d4d0d4b936ccea75ef99fb58f
Sara Raley <sara.m.raley@gmail.com>
2023-01-08 12:05:02 -0700

    adds forecast model and associations with trails

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,5 +1,6 @@
 class Trail < ApplicationRecord
   has_many :conditions
+  has_many :forecasts
   has_many :fast_facts
   has_many :hike_schedules
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_015808) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_190426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_015808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "trail_id"
+  end
+
+  create_table "forecasts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "hike_schedules", force: :cascade do |t|
