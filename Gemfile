8d30dfb57841353424e02951de99ea252784fc13
Sara Raley <sara.m.raley@gmail.com>
2023-01-08 12:28:59 -0700

    adds forecast controller index function and http gem

--- Gemfile
+++ Gemfile
@@ -48,3 +48,4 @@ group :development do
 end
 
 gem 'jwt'
+gem "http"
---
+++ app/controllers/forecasts_controller.rb
@@ -0,0 +1,26 @@
+class ForecastsController < ApplicationController
+  def index
+    ### SUNRISE/SUNSET/FIRST LIGHT/LAST LIGHT/DAWN/DUSK/GOLDEN HR/DAY LENGTH
+
+    light = HTTP.get("https://api.sunrisesunset.io/json?lat=40.06892&lng=-105.6193&date=today").parse(:json)
+
+    sunrise = light["results"]["sunrise"]
+    sunset = light["results"]["sunset"]
+    first_light = light["results"]["first_light"]
+    last_light = light["results"]["last_light"]
+    golden_hour = light["results"]["golden_hour"]
+
+    ### WEATHER
+    weather = HTTP.get("https://api.weather.gov/gridpoints/BOU/41,77/forecast").parse(:json)
+
+    weather = ["properties"]["periods"]
+
+    today_temp = weather["properties"]["periods"][0]["temperature"]
+    today_wind_speed = weather["properties"]["periods"][0]["windSpeed"]
+    today_wind_direction = weather["properties"]["periods"][0]["windDirection"]
+    today_forecast = weather["properties"]["periods"][0]["detailedForecast"]
+
+    render json: light
+    render json: weather
+  end
+end
